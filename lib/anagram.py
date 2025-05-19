f9fc5ca241e5156ab075e0cd13a20ba241c986df
Celestine Mecheo <celestinenyamoita094@gmail.com>
2025-05-19 18:49:19 +0300

    Complete Anagram Detector Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        matches = []
+        for w in word_list:
+            if sorted(w) == sorted(self.word) and w.lower() != self.word.lower():
+                matches.append(w)
+        return matches
